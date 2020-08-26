224a1b07b47e2fdaa2f0c0aced4167c7b4a99e2c
Isiko Saidiali <54436062+SaidiAli@users.noreply.github.com>
2020-08-26 09:21:20 +0300

    Added basic authentication and set up passport (#10)
    * Added basic authentication and set up passport
    
    * Refactor

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+use Illuminate\Validation\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Register a user into the application
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function register(Request $request) {
+        try {
+            $validator = $this->validator($request);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'data' => [
+                        'errors' => $validator->errors()
+                    ]
+                ]);
+            }
+
+            $data = $validator->validated();
+
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password'])
+            ]);
+
+            $token = $user->createToken('user-' . $user->id)->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'user' => $user,
+                    'token' => $token
+                ]
+            ]);
+        } catch (\Exception $e) {
+            Log::error('An error occured while trying to register a user: ' . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'data' => [
+                    'errors' => 'Registration failed, please try again'
+                ]
+                ], 500);
+        }
+    }
+
+    /**
+     * Validate request
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    private function validator(Request $request) {
+        return FacadesValidator::make($request->only(['name', 'email', 'password']), [
+            'name' => 'required',
+            'email' => 'email|required',
+            'password' => 'required|min:8'
+        ]);
+    }
+}
